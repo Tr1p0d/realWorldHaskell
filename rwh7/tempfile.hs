0c1e19d64a1bbe7fde716568f1dadb9d8a838161
John Anderson <sontek@gmail.com>
2013-08-16 17:47:32 +0200

    neconeco

--- rwh7/tempfile.hs
+++ rwh7/tempfile.hs
@@ -3,27 +3,37 @@ import System.Directory (getTemporaryDirectory, removeFile)
 import System.IO.Error (catch)
 import Control.Exception (finally)
 
-main :: IO
+main :: IO () 
 main = withTempFile "myTemp.txt" myAction
 
-tellPosition :: Handle -> FilePath -> IO
+tellPosition :: Handle -> FilePath -> IO String
 tellPosition h p = do
 	pos <- hTell h
-	putStrLn "our position on file : " ++ p ++ " is : " ++ pos
+	return $ "our position on file : " ++ p ++ " is : " ++ (show pos)
 	
 
-myAction :: FilePath -> Handle -> IO
+myAction :: FilePath -> Handle -> IO ()
 myAction tempname temph = do
 	putStrLn "welcome to write tmpFile.hs"
-	tellPosition temph tempname
+	tmp <- tellPosition temph tempname
+	putStrLn tmp
 
 	putStrLn "lets write something"
-
+	hPutStrLn temph $ show [1..100]
 	putStrLn "written"
+
+	tmp <- tellPosition temph tempname
+	putStrLn tmp
+	putStrLn "done"
+
 	
 withTempFile :: FilePath -> ( FilePath -> Handle -> IO a ) -> IO a
 withTempFile pattern func = do
-	
+	tempdir <- catch (getTemporaryDirectory) (\_ -> return ".")	
+	(tempfile, temph) <- openTempFile tempdir pattern
+	finally (func tempfile temph) (do 
+		hClose temph
+		removeFile tempfile)
 
 
 
