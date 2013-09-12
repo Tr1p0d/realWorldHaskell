a955e572603c88743f4c22310ed2df6ba614b06b
Marek Tr1p0d Kidon <marek.kidon@gmail.com>
2013-09-12 23:30:05 +0200

    foldir

---
+++ rwh9/FolDir.hs
@@ -0,0 +1,7 @@
+data Iterate seed =   
+	Done 		{ unwrap :: seed }
+	| Skip		{ unwrap :: seed }	
+	| Continue	{ unwrap :: seed }
+		deriving (Show)
+
+type Iterator seed = seed -> Info -> Iterate seed
