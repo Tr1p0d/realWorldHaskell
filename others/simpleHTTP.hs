c7cdde117ef35c3126f83f2fab1091f4e4f3fa2a
Marek Tr1p0d Kidon <marek.kidon@gmail.com>
2013-10-05 01:13:15 +0200

    web client rdy

--- others/simpleHTTP.hs
+++ others/simpleHTTP.hs
@@ -1,6 +1,41 @@
 import Network.HTTP
+import Network.URI
+import Data.Maybe
+import Network.Stream
 
-testRide url = simpleHTTP req 
-where
-	req = mkRequest GET parsedUri
-	parsedUri = parseURI url 
+data HTTPErrorStructure = 
+	ConnectionError |
+	ParseError		|
+	UnknownError	|
+	Redirect		|
+	NotFound		
+
+testRide :: String -> IO String
+testRide url = do
+	a <- simpleHTTP req
+	case a of
+		Left ErrorReset -> return "asd"--(ConnectionError)
+		Left ErrorClosed ->  return "asd"--(ConnectionError)
+		Left (ErrorParse a) -> return "asd"--( ParseError)
+		Left (ErrorMisc a) ->  return "asd"--( UnknownError)
+		otherwise -> do
+			b <- getResponseCode a
+			case b of
+				(3,_,_) -> 
+					case lookupHeader HdrLocation (fromJust responseHeaders) of
+						Just c -> testRide c
+						Nothing -> return "got redirect but cannot redirect"
+					where 	
+						responseHeaders = 
+							case a of
+								Left x -> Nothing
+								Right n -> Just (rspHeaders n)
+				(2,_,_) -> do (getResponseBody a)
+				(4,_,_) -> return "notfound"-- ( NotFound)
+	where
+		req = mkRequest GET parsedUri
+		parsedUri = fromJust $ parseURI url 
+
+
+
+-- wisLogin user pass url =
